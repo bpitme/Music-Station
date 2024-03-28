bd69372eeaab0391822a8f31ee12124bd74cc729
bpitme <128807311+bpitme@users.noreply.github.com>
2024-03-28 03:34:35 +0000

    1st commint

---
+++ Music Station/Music Station/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+//Function to be called onmouseover and onmouseout to change the color of " Welcome to Music Station"
+function newcolor() {
+    document.getElementById("Welcome_to Music Station").style.color = "Pink";
+}
+function oldcolor() {
+    document.getElementById("Welcome_to Music Station").style.color = "darkred";
+}
+
+    
+
+
+
